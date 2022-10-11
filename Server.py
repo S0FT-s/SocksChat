781c3cb0d0a3105f5e3350138a349599cd4c01ae
Soft <76917904+S0FT-s@users.noreply.github.com>
2022-10-11 20:26:15 +0100

    New id system Work in progress

--- Server.py
+++ Server.py
@@ -4,7 +4,6 @@ from threading import *
 
 Host = '127.0.0.1'
 Port = 1234
-Clients_names = []
 id = 0
 id_List = []
 
@@ -14,9 +13,16 @@ ServerSocket.bind((Host,Port))
 
 
 
+
 def Client_Handler(conn, addr):
     #o client vai mandar o seu nome primeiro e depois sim pode começar o chat
-    print(f"New Client connected")
+    global id
+    Newid = id + 1
+    id_List.append(Newid)
+    id = id + 1
+    print(id)
+    print(f"New Client connected from {addr}, recive the id of {Newid}")
+    print(id_List)
     while True:
         #Aqui o client vai mandar as suas msg para o server que depois o mesmo vai enviar para todos.
         msg = conn.recv(1024)
@@ -32,4 +38,5 @@ def ServerStart():
         conn, addr = ServerSocket.accept()
         th1 = Thread(target=Client_Handler, args=(conn,addr))
         th1.start()    
+
 ServerStart()
