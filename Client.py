3ff0f7a4d878edb38b94d126ec8b4aebe4343066
Soft <76917904+S0FT-s@users.noreply.github.com>
2022-10-30 00:54:50 +0100

    Client recive and Server sends
    I new code to send the message to everyone and mostrar as mensagens instantaneamente

--- Client.py
+++ Client.py
@@ -1,13 +1,25 @@
 import socket
+from threading import *
 
 #For now is hardcode later will not be
 Server_IP   = '127.0.0.1'
 Server_Port =  1234
-NickName    = input("Chose your nickname")
+NickName    = input("Chose your nickname\n -->")
+
+
+def MsgReciver(client):
+    while True:
+        recive_msg = client.recv(1024).decode('utf-8')
+        print(recive_msg)
+    
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client:
     client.connect((Server_IP, Server_Port))
+    client.send(NickName.encode('utf-8'))
+    id = client.recv(64).decode('utf-8')
+    th1 = Thread(target=MsgReciver, args=(client, ))
+    th1.start()
     while True:
         MSG = str(input())
-        client.send(f"<{NickName}> {MSG}".encode("utf-8"))
-        client.recv(1024)
+        client.send(f"<{NickName} {id}> {MSG}".encode("utf-8"))
+            
--- Server.py
+++ Server.py
@@ -2,40 +2,55 @@
 import socket 
 from threading import *
 
-Host = '127.0.0.1'
-Port = 1234
-id = 0
-id_List = []
+Host        = '127.0.0.1'
+Port        = 1234
+id          = 0
+Newid       = 0
+clients     = []
+id_List     = []
+NicksList   = []
 
 
 ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ServerSocket.bind((Host,Port))
 
+#MsgSender is going to send the msg for all the clients
+def MsgSender(msg):
+    for conn in clients:
+        conn.send(msg)
 
+def NamesIds(conn):
+    #id Part
+    global id
+    global Newid
+    Newid = id + 1
+    id_List.append(Newid)
+    id = id + 1
+    conn.send(f"{id}".encode('utf-8'))
+    #Name Part
+    global Names
+    name  = conn.recv(1024)
+    NicksList.append(name.decode('utf-8'))
 
 
 def Client_Handler(conn, addr):
     #o client vai mandar o seu nome primeiro e depois sim pode começar o chat
-    global id
-    Newid = id + 1
-    id_List.append(Newid)
-    id = id + 1
-    print(id)
+    NamesIds(conn)
     print(f"New Client connected from {addr}, recive the id of {Newid}")
-    print(id_List)
     while True:
         #Aqui o client vai mandar as suas msg para o server que depois o mesmo vai enviar para todos.
         msg = conn.recv(1024)
-        conn.send(msg)
         msg_Decode = msg.decode('utf-8')
         print(f"{msg_Decode}")
-        
-        
+        MsgSender(msg)
+            
+              
 def ServerStart():
     ServerSocket.listen(5)
     print("Server is now listening")
     while True:
         conn, addr = ServerSocket.accept()
+        clients.append(conn)
         th1 = Thread(target=Client_Handler, args=(conn,addr))
         th1.start()    
 
